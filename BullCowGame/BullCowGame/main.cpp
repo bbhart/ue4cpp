55a080369c22d745cc585de75e55cdff685837be
Brian Hart <bbhart@bbhart.com>
2019-05-07 21:28:53 -0700

    Sleepy. Checkpoint.

--- BullCowGame/BullCowGame/main.cpp
+++ BullCowGame/BullCowGame/main.cpp
@@ -8,8 +8,42 @@
 
 #include <iostream>
 
+using namespace std;
+constexpr int WORD_LENGTH = 5;
+constexpr int MAX_TRIES = 10;
+string Guess;
+
+void PrintIntro();
+string GetGuess();
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    PrintIntro();
+    
+    for (int i = 0; i < MAX_TRIES; i++)
+    {
+        Guess = GetGuess();
+        
+    }
+    cout << "You guessed " << Guess << endl;
+
     return 0;
+}
+
+void PrintIntro()
+{
+    
+    cout << "Welcome to Bulls and Cows, the fun letter guessing game." << endl;
+    cout << "Can you guess the " << WORD_LENGTH << " isogram I'm thinking of?" << endl;
+    
+}
+
+string GetGuess()
+{
+    
+    cout << "Make a guess: " << endl;
+    getline(cin, Guess);
+
+    return Guess;
+
 }
